adb1656da00a7bd404a1fc44ddf0167a9edc785b
Portus89 <148595123+Portus89@users.noreply.github.com>
2023-12-16 00:05:55 +0000

    Add files via upload

---
+++ Examen-Transversal.py
@@ -0,0 +1,6 @@
+Examen-Transversal
+Integrantes = ["-Victor Portus", "-Jeremias Castro"]
+print("LIsta de nombres y apellido de los integrantes del grupo":)
+    for integrantes in integrantes:
+        print(integrantes)
+        
